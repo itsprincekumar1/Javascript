0ac7316f9aa20bb477d0e56b682453c7977e9dc6
Prince <itsprincekumar19@gmail.com>
2025-12-12 11:43:47 +0000

    Undefined vs Not Defined in JS

---
+++ 01_basic/undefinedVsNotDefined.js
@@ -0,0 +1,13 @@
+var a;  // undefined
+console.log(a);
+
+var b = 10; // defined
+console.log(b);
+
+console.log(c); //not defined => this will throw error
+
+/*
+Undefined : This act as a placeholder. There is some memeory space assigned to the variable but no value. THIS IS NOT EQUAL TO EMPTY.
+Defined : This is a complete variable which has memory as well as values assigned to it.
+Not Defined : Variables which are not even decalared in the program which means they do not have any memory assigned to it and they are not yet part of the program
+*/
