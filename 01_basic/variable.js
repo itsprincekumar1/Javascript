ff2648d4ab4305395b76362c3aa5a9e6253039a1
Prince <itsprincekumar19@gmail.com>
2025-12-12 11:39:24 +0000

    Variables in JS

---
+++ 01_basic/variable.js
@@ -0,0 +1,20 @@
+var a = 10;     // A variable with integer value
+var country = "India";  // A variable with string value
+console.log(a);
+console.log(country);
+/*
+Variable in JS are containers which store some values
+- These are dynamically typed which means we do not define the 
+type of variable it decide it while runtime
+*/
+// Dynamically typed example
+var vari = 100; // vari is storing an 100 which is an integer
+vari = "Prince" // vari is now storing "Prince" which is an string
+// The above change take place during runtime
+
+/* 
+- The variables in JS has no type instead the values has the types
+- Integers, string, booleans these all are the types of values that can be stored
+in a variable inside JS
+*/
+var isAvailable = false;    // Boolean value
