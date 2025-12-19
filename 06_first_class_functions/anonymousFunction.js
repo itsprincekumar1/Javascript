9d6dbeb8d97e50a91aa2b6f14c5bfd84b8a10cd0
Prince <itsprincekumar19@gmail.com>
2025-12-19 20:16:30 +0000

    05 Function basics

---
+++ 06_first_class_functions/anonymousFunction.js
@@ -0,0 +1,8 @@
+//Functions who do not have their own name or identity
+var b = function ()
+{
+    console.log("b called");
+}
+
+// here function assigned to b has no name and these type of functions are known as anonymous function
+// Anonymous functions cannot be declared as function statement as they will throw refernece error 
