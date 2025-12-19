a14c42828fecd3004105b44c6642eb38a3a540a8
Prince <itsprincekumar19@gmail.com>
2025-12-19 20:14:22 +0000

    02 Function Basic

---
+++ 06_first_class_functions/functionExpression.js
@@ -0,0 +1,5 @@
+// Function expression
+var b = function (){
+    console.log("b called");
+}
+// In JS we can assign a function to a variable as a value and this type of function are known as function expression.
