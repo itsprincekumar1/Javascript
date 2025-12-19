9aa5795c6a9e4b602c38ce311cf4174fec4f01be
Prince <itsprincekumar19@gmail.com>
2025-12-19 20:15:24 +0000

    03 Function Basic

---
+++ 06_first_class_functions/parametersAndArguments.js
@@ -0,0 +1,11 @@
+function a(param1, param2){
+    console.log("The sum of numbers are", param1+param2);
+}
+
+a(2,3);
+
+// Parameters : The placeholder assigned while declaring a function is known as parameters. They defines how many values are required while
+// calling the function and they act like a local variable(inside the functions local scope) for that function 
+
+// Arguments : The values passed to the function while calling them are known as arguments, these values takes the place of parameters and
+// initialize them with the passed value.
